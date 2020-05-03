510957318a5fcc7927e468e3567de0781239b653
Alan <faeralan@gmail.com>
2020-05-03 19:28:50 -0300

    Agregar archivos de proyecto.

---
+++ BilleteraVirtual/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BilleteraVirtual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
